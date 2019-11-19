423e058af1aab5126d150f718e3e8a2d87117ba0
wikkiewikkie <wikkiewikkie@gmail.com>
2019-11-19 09:59:56 -0500

    add zip tools

---
+++ zip_file_list.py
@@ -0,0 +1,17 @@
+import os
+import zipfile
+
+with open("files.txt", "w", encoding="utf8") as out_file:
+
+    files = os.listdir(".")
+    
+    for file_name in files:
+
+        if file_name[-3:] == "zip":
+            print(file_name)
+            zip_file = zipfile.ZipFile(file_name, "r")
+
+            for zf_name in zip_file.namelist():
+                out_file.write("{}{}".format(zf_name, "\n"))
+            
+
---
+++ zip_file_size.py
@@ -0,0 +1,18 @@
+import os
+import zipfile
+
+size = 0
+
+files = os.listdir(".")
+
+for file_name in files:
+
+    if file_name[-3:] == "zip":
+        print(file_name)
+        zip_file = zipfile.ZipFile(file_name, "r")
+
+        for zf in zip_file.infolist():
+            size += zf.file_size
+           
+
+print(size/1024/1024/1024)
